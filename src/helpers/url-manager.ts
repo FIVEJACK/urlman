e9a848c02b25389723e8e0b9dc00b8901d0aa8ed
Edinofri Karizal Caniago <edinofri.caniago@bukalapak.com>
2023-07-10 12:13:14 +0700

    only allow whitelisted url

--- src/helpers/url-manager.ts
+++ src/helpers/url-manager.ts
@@ -1,15 +1,17 @@
 import fetch from 'node-fetch';
-import { UrlWithParsedQuery } from 'url';
+import { UrlWithParsedQuery, parse as parseUrl } from 'url';
 import ReturnObject from 'Helpers/return-object';
 
 // Fetch url content and return the content
 export const urlManager = async (parsedUrl: UrlWithParsedQuery) => {
   const toReturn = new ReturnObject;
   toReturn.setCSP('default-src *;');
-  const redirectUrl = parsedUrl?.query?.u || '';
+  const redirectUrl: any = parsedUrl?.query?.u || '';
   const isValidUrl = (redirectUrl.indexOf('http://') === 0 || redirectUrl.indexOf('https://') === 0);
+  const proxyHosts = ['analytics.tiktok.com', 'connect.facebook.net'];
+  const hostname = parseUrl(redirectUrl).hostname ?? '';
 
-  if (isValidUrl) {
+  if (isValidUrl && proxyHosts.indexOf(hostname) >= 0) {
     try {
       const urlResponse = await fetch(redirectUrl as string);
       const content = await urlResponse.text();
